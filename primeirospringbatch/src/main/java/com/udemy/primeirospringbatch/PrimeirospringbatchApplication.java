bd68aa55ee50e430cd021a9598cb1d0ac094e52f
Norberto Filho <norberto.santos@gmail.com>
2024-02-08 15:33:25 -0300

    Arquivos do primeiro spring batch do curso Jobs com Spring Batch

---
+++ primeirospringbatch/src/main/java/com/udemy/primeirospringbatch/PrimeirospringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeirospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirospringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirospringbatchApplication.class, args);
+	}
+
+}
---
+++ primeirospringbatch/src/test/java/com/udemy/primeirospringbatch/PrimeirospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeirospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
