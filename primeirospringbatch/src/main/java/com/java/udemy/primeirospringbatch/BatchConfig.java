5e02452f5e738995c042459fc74339a2836846eb
Norberto Filho <norberto.santos@gmail.com>
2024-02-08 16:22:47 -0300

    Alterações no Job e no Step para a versão do Spring Batch mais nova

--- primeirospringbatch/src/main/java/com/java/udemy/primeirospringbatch/BatchConfig.java
+++ primeirospringbatch/src/main/java/com/java/udemy/primeirospringbatch/BatchConfig.java
@@ -3,36 +3,28 @@ package com.java.udemy.primeirospringbatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
+
 @Configuration
 public class BatchConfig {
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;	
-	
+		
 	@Bean
 	public Job imprimeOlaJob(){
-		return jobBuilderFactory.
-				get("imprimeOlaJob").
+		return new JobBuilder("imprimeOlaJob").
 				start(imprimeOlaStep()).
 				build();
     	
     }
 	
 	public Step imprimeOlaStep() {
-		return StepBuilderFactory.
-				get("imprimeOlaStep").
+		return new StepBuilder("imprimeOlaStep").
 				tasklet(new Tasklet() {
 					
 					@Override
